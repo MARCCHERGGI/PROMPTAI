62fbdfe2c672ff37b9c3b58aa32889092fec61d2
MARCCHERGGI <161167689+MARCCHERGGI@users.noreply.github.com>
2025-06-13 03:48:44 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-screen bg-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-black">✅ PromptAI is back</h1>
+    </div>
+  );
+}
